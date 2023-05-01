5e4d4c891005280486f167291de7d6f11b1645e0
Alina Inchaustegui <alinainchaustegui@gmail.com>
2023-04-30 21:36:21 -0400

    book reviews push

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://alinainch:Veggie123@cluster0.olzdjfg.mongodb.net/books?retryWrites=true&w=majority', 
+    'dbName': 'books'
+};
